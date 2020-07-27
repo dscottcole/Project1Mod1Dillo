2428c51fb0af8d458397645452f9e324d089b8e4
dscottcole <67556122+dscottcole@users.noreply.github.com>
2020-07-27 15:59:48 -0500

    created basic tables, Items, Orders

---
+++ db/migrate/20200727203111_create_items.rb
@@ -0,0 +1,14 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string :item_name
+      t.string :user_id
+      t.string :order_id
+      t.string :location #austin, dallas, houston
+      t.string :category 
+      t.string :condition #new, used_like_new, used_fair, used_not_great
+      t.float :price
+      t.text :description, limit: 1000
+    end
+  end
+end
