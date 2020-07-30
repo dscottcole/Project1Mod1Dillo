0f667afcc8149be2cd392c1278e44035f957fcd9
dscottcole <67556122+dscottcole@users.noreply.github.com>
2020-07-30 15:41:43 -0500

    finishing touches

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,53 +5,57 @@ class User < ActiveRecord::Base
     $cred_hash = {}
 
     def get_list
-        $prompt.ask("What item would you like to list in the marketplace?") do |q|
+        $prompt.ask("What item would you like to list in the marketplace?".light_yellow.bold) do |q|
             q.required true
             q.modify :strip
         end
     end
     def get_cat
-        $prompt.select("What category best describes your item?", $cat, required: true)
+        $prompt.select("What category best describes your item?".light_yellow.bold, $cat, required: true)
     end
     def get_cond
-        $prompt.select("What is your item's condition?", $cond, required: true)
+        $prompt.select("What is your item's condition?".light_yellow.bold, $cond, required: true)
     end
     def get_price
-        $prompt.ask("How much $ do you want for your item?") do |q|
+        $prompt.ask("How much $ do you want for your item?".light_yellow.bold) do |q|
             q.required true
             q.convert :float
             q.messages[:convert?] = "Please enter a number."
         end
     end
     def get_desc
-        $prompt.ask("Please provide a description for your item.", required: true)
+        $prompt.ask("Please provide a description for your item.".light_yellow.bold, required: true)
     end
     def get_s_address 
-        $prompt.ask("Please provide a shipping address.", required: true)
+        $prompt.ask("Please provide a shipping address.".light_yellow.bold, required: true)
     end
     def get_m_address 
-        result = $prompt.ask("Please provide a meeting address.", required: true, convert: :string)
+        result = $prompt.ask("Please provide a meeting address.".light_yellow.bold, required: true, convert: :string)
     end
     def get_date_time
-        $prompt.ask("Provide a date and time for the meet-up with #{Date.today + 1} being the earliest meet-up date. (Format: YYYY/MM/DD HH:MM)", convert: :time, required: true)
+        $prompt.ask("Provide a date and time for the meet-up with #{Date.today + 1} being the earliest meet-up date. (Format: YYYY/MM/DD HH:MM)".light_yellow.bold) do |q|
+            q.required true
+            q.convert :time
+            q.messages[:convert?] = "Invalid date format."
+        end
     end
     def get_item
         Item.generate_list
-        $prompt.select("What item would you like to purchase?", $item_array, required: true)
+        $prompt.select("What item would you like to purchase?".light_yellow.bold, $item_array, required: true)
     end
     def get_your_items
         Item.generate_list
-        $prompt.select("What item would you like to modify the price for?", $user_items, required: true)
+        $prompt.select("What item would you like to modify the price for?".light_yellow.bold, $user_items, required: true)
     end
     def shipvlocal
-        $prompt.select("Would this item be shipped or will it be picked up locally?", %w(Shipment Local), required: true)
+        $prompt.select("Would this item be shipped or will it be picked up locally?".light_yellow.bold, %w(Shipment Local), required: true)
     end
     def get_new_price
-        $prompt.ask("What would you like to change the price to?", required: true)
+        $prompt.ask("What would you like to change the price to?".light_yellow.bold, required: true)
     end
 
     def get_order_type
-        $prompt.select("Would you like to view your Sales or Purchases?", $salesvpurch, required: true)
+        $prompt.select("Would you like to view your Sales or Purchases?".light_yellow.bold, $salesvpurch, required: true)
     end
      
     def list
@@ -69,17 +73,17 @@ class User < ActiveRecord::Base
         b = a.find_all {|i| i == self.id}
 
         if a.length == b.length
-            puts "========================================"
-            puts "You own all the items in the marketplace."
-            puts "========================================"
-            list_browse_purchase
+            puts "========================================".red.bold
+            puts "You own all the items in the marketplace.".white.bold
+            puts "========================================".blue.bold
+            marketplace_menu
         end
         
         item_selection = get_item.item_name
         item = Item.all.find_by(item_name: item_selection)
 
         until self.id != item.user_id do
-            puts "You cannot purchase your own item."
+            puts "You cannot purchase your own item.".red.bold
             item_selection = get_item.item_name
             item = Item.all.find_by(item_name: item_selection)
         end        
@@ -90,23 +94,30 @@ class User < ActiveRecord::Base
             address = get_m_address
             order_t = "Local"
             datime = get_date_time
+binding.pry
+            until datime >= Date.today + 1 == true do
+                puts "The date entered is not after #{Date.today}. Please revise.".white.bold
+                datime = get_date_time
+            end
+
             new_order = Order.create(seller: item.user_id, buyer: self.id, item_id:item.id, order_type: order_t, shipping?: false, shipping_address: nil, meeting_location: address, date_time: datime)
             Item.all.find_by(id: item.id).update(order_id: new_order.id)
-            puts "Thank you for your purchase! The seller will contact you via email if there are any issues with the meet-up time/date."
-            puts "Seller Information:"
-            puts "#{item.user.name}"
-            puts "#{item.user.email}"
-            puts "Please contact seller with any additional questions."          
+            puts "Thank you for your purchase! The seller will contact you via email if there are any issues with the meet-up time/date.".white.bold
+            puts "Seller Information:".light_yellow.bold
+            puts "#{item.user.name}".white
+            puts "#{item.user.email}".white
+            puts "Please contact seller with any additional questions.".white.bold
+
         elsif ship_vs_local == "Shipment"
             address = get_s_address
             order_t = "Shipment"
             new_order = Order.create(seller: item.user_id, buyer: self.id, item_id:item.id, order_type: order_t, shipping?: true, shipping_address: address, meeting_location: nil, date_time: nil)
             Item.all.find_by(id: item.id).update(order_id: new_order.id)
-            puts "Thank you for your purchase! Expect to receive an email with a tracking number and additional information."
-            puts "Seller Information:"
-            puts "#{item.user.name}"
-            puts "#{item.user.email}"
-            puts "Please contact seller with any additional questions."
+            puts "Thank you for your purchase! Expect to receive an email with a tracking number and additional information.".white.bold
+            puts "Seller Information:".light_yellow.bold
+            puts "#{item.user.name}".white
+            puts "#{item.user.email}".white
+            puts "Please contact seller with any additional questions.".white.bold
         end
     end
 
@@ -123,68 +134,75 @@ class User < ActiveRecord::Base
         a = $available_items.map {|i| i.user_id}
        
         if  a.any?(self.id) == false
-            puts "===================================================".light_red.bold
+            puts "===================================================".red.bold
             puts "You do not own any of the items in the marketplace.".white.bold
-            puts "===================================================".light_blue.bold
-            list_browse_purchase
+            puts "===================================================".blue.bold
+            marketplace_menu
         else
             self.get_user_items
             Item.generate_list
             int_item = get_your_items
             item = Item.all.find_by(id: int_item.id)
-            puts "The current item price is: #{item.price}"
+            puts "The current item price is: #{item.price}".white.bold
             new_price = get_new_price
             Item.all.find_by(id: int_item.id).update(price: new_price)
-            puts "Item price is now #{Item.all.find_by(id: int_item.id).price}"
+            puts "Item price is now #{Item.all.find_by(id: int_item.id).price}".light_green.underline
         end
     end
 
     def view_orders
+        system "clear"
         type = get_order_type
         if type == "Sales"
-            puts "Sales:"
+            puts "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++".red.bold
+            puts "                                    Sales                                ".white.bold
+            puts "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++".blue.bold
             order_array = Order.all.where(seller: self.id)
             if order_array.any? == false
-                puts "You have no sales to display."
+                puts "You have no sales to display.".red.bold
             else
                 order_array.each do |o|
                     puts "============================================================".red.bold
-                    puts "Order type:"
+                    puts "Order type:".light_yellow.bold
                     puts o.order_type
-                    puts "Buyer Name:"
+                    puts "Buyer Name:".light_yellow.bold
                     puts User.all.find_by(id: o.buyer).name
-                    puts "Buyer Email:"
+                    puts "Buyer Email:".light_yellow.bold
                     puts User.all.find_by(id: o.buyer).email
-                    puts "Item Name:"
+                    puts "Item Name:".light_yellow.bold
                     puts o.item.item_name
-                    puts "Item Condition:"
+                    puts "Item Condition:".light_yellow.bold
                     puts o.item.condition
-                    puts "Item Price:"
+                    puts "Item Price:".light_yellow.bold
                     puts o.item.price
                     puts "============================================================".blue.bold
+                    sleep 0.75
                 end
             end
         elsif type == "Purchases"
-            puts "Purchases:"
+            puts "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++".red.bold
+            puts "                                   Purchases                              ".white.bold
+            puts "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++".blue.bold
             order_array = Order.all.where(buyer: self.id)
             if order_array.any? == false
-                puts "You have no purchases to display."
+                puts "You have no purchases to display.".red.bold
             else
                 order_array.each do |o|
                     puts "============================================================".red.bold
-                    puts "Order type:"
+                    puts "Order type:".light_yellow.bold
                     puts o.order_type
-                    puts "Seller Name:"
+                    puts "Seller Name:".light_yellow.bold
                     puts o.item.user.name
-                    puts "Seller Email:"
+                    puts "Seller Email:".light_yellow.bold
                     puts o.item.user.email
-                    puts "Item Name:"
+                    puts "Item Name:".light_yellow.bold
                     puts o.item.item_name
-                    puts "Item Condition:"
+                    puts "Item Condition:".light_yellow.bold
                     puts o.item.condition
-                    puts "Item Price:"
+                    puts "Item Price:".light_yellow.bold
                     puts o.item.price
                     puts "============================================================".blue.bold
+                    sleep 0.75
                 end
             end
         end
